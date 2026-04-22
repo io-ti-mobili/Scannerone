3b6711e159d7fa470d8f6d2ee0057ff0e52efb3c
davide <26660577+sonodavide@users.noreply.github.com>
2026-04-22 11:01:45 +0200

    icone stock material design

--- app/src/main/java/com/example/scannerone/ui/AppDestination.kt
+++ app/src/main/java/com/example/scannerone/ui/AppDestination.kt
@@ -11,10 +11,10 @@ enum class AppDestination(
     val icon: Int,
 ) {
     HOME("Home", R.drawable.ic_home),
-    DATABASESCREEN("Database", R.drawable.database),
-    WIFISCAN("WifiScan", R.drawable.wifi),
-    MAP("Map", R.drawable.map),
-    RIEPILOGO("Riepilogo", R.drawable.report),
-    SETTINGS("Settings", R.drawable.settings)
-   
+    DATABASESCREEN("Database", R.drawable.outline_database_24),
+    WIFISCAN("WifiScan", R.drawable.outline_android_wifi_3_bar_24),
+    MAP("Map", R.drawable.outline_map_24),
+    RIEPILOGO("Riepilogo", R.drawable.outline_edit_document_24),
+    SETTINGS("Settings", R.drawable.outline_build_24)
+
 }
